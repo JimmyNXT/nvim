0a31a3e64a86b0fa4ea1be35add299fab866d4f9
Dean Minnie <dminnie33@gmail.com>
2024-06-14 07:22:58 +0200

    Vim Fugitive add

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -98,6 +98,13 @@ return {
     end,
   },
   {
-    "tpope/vim-fugitive"
-  }
+    "tpope/vim-fugitive",
+    cmd = {"Git"},
+    event = "VeryLazy",
+    dependencies = {
+      "tpope/vim-rhubarb",
+      "tpope/vim-obsession",
+      "tpope/vim-unimpaired",
+    },
+  },
 }
